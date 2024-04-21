0541634e634d09a881c7b605882bf8166a0aa95f
prayush21 <prayushdave@gmail.com>
2024-04-21 18:27:42 +0530

    Complete Axios and Redux Setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StoreProvider from "@/lib/storeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Weather App",
-  description: "gives the latest updates in weather",
-};
+// export const metadata = {
+//   title: "Weather App",
+//   description: "gives the latest updates in weather",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-200`}>{children}</body>
+      <StoreProvider>
+        <body className={`${inter.className} bg-gray-200`}>{children}</body>
+      </StoreProvider>
     </html>
   );
 }
